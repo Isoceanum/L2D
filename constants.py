2c1891e787c4df59c68fffa33a9d7cb0aa71b2fc
Isoceanum <Isoceanum@outlook.com>
2025-05-01 14:01:39 +0200

    change parameters

--- constants.py
+++ constants.py
@@ -70,7 +70,7 @@ L2D_RAY_LENGTH = 20.0  # Max raycast length in meters
 L2D_OBSERVATION_SIZE = 10
 
 # --- L2D Reward Weights ---
-L2D_SPEED_REWARD_WEIGHT = 0.1     # Reward per m/s of forward velocity
+L2D_SPEED_REWARD_WEIGHT = 0.05     # Reward per m/s of forward velocity
 L2D_TIME_PENALTY = 0.1            # Constant penalty per step (time cost)
 L2D_CENTERING_REWARD_WEIGHT = 0.5   # Centering reward weight
 L2D_WALL_PROXIMITY_PENALTY_WEIGHT = 1 # Penalty for being close to walls
